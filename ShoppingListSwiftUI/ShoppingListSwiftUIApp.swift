515b3498bc8e8039d2fd66513a5b08d78f6e1263
leehs81 <leehs81@brandi.co.kr>
2022-05-25 22:39:41 +0900

    Initial Commit

---
+++ ShoppingListSwiftUI/ShoppingListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListSwiftUIApp.swift
+//  ShoppingListSwiftUI
+//
+//  Created by hoseung Lee on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
